e33d4042176a5fb9ecbe4a4a54095c14f2b040d6
FrancescoCauzzi <elblanco.fc@gmail.com>
2023-09-29 14:47:52 +0200

    feat: migration done

---
+++ la-mia-pizzeria-crud-mvc/Database/PizzeriaContext.cs
@@ -0,0 +1,17 @@
+﻿using la_mia_pizzeria_crud_mvc.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace la_mia_pizzeria_crud_mvc.Database
+{
+    public class PizzeriaContext : DbContext
+    {
+        public DbSet<Pizza> Pizzas { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=localhost;Initial Catalog=MVCEFPizzeria;" +
+            "Integrated Security=True;TrustServerCertificate=True");
+        }
+
+    }
+}
---
+++ la-mia-pizzeria-crud-mvc/Models/Pizza.cs
@@ -0,0 +1,44 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace la_mia_pizzeria_crud_mvc.Models
+{
+    public class Pizza
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [MaxLength(100)]
+        public string Name { get; set; }
+
+        [Column(TypeName = "text")]
+        public string Description { get; set; }
+
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal Price { get; set; }
+
+        [MaxLength(500)]        
+
+        public string ImageUrl { get; set; } = "/images/default_pizza.png";
+
+
+
+        public Pizza(string name, string description,decimal price, string imageUrl)
+        {
+            Name = name;
+            Description = description;
+            Price = price;
+            ImageUrl = imageUrl;
+        }
+        // constructor overload
+        public Pizza(string name, string description, decimal price)
+        {
+            Name = name;
+            Description = description;
+            Price = price;
+            // ImageUrl will use the default value string.Empty
+        }
+
+
+    }
+}
